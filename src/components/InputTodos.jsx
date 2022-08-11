e0e1735188d39e92c249c3c21b9bf1e07bdbd4e8
Isaac <isaac.ruiz0219@gmail.com>
2022-08-11 11:36:53 -0700

    adding get request

--- src/components/InputTodos.jsx
+++ src/components/InputTodos.jsx
@@ -1,22 +1,34 @@
 import React, {Fragment, useState} from "react";
 
 function InputTodos() {
-const [description, setDescription] = useState("");
+    const [description, setDescription] = useState("");
 
-  return (
+    const onSubmitForm = async (e) =>{
+        e.preventDefault();
+
+        try {
+            const body = description
+            await fetch()
+        }catch(err){
+            console.log(err)
+        }
+
+    }
+
+    return (
     <div>
         <h1 className="text-center my-5">Input Todo</h1>
         <form className="d-flex">
             <input type="text" placeholder='Add Todo' className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
             <button className="btn btn-success">
-               Add
-           </button>             
+                Add
+            </button>             
         </form>
 
     </div>
 
 
-  )
+    )
 }
 
 export default InputTodos
